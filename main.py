95826c1b6795315177baee9801db8c724a8d0b28
Riley Driscoll <rileydriscoll97@gmail.com>
2026-02-22 15:11:35 -0800

    black formatting

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "Welcome to the API"}
 
+
 @app.get("/random")
 def random_number():
-    return random.randint(0,20)
+    return random.randint(0, 20)
+
 
 if __name__ == "__main__":
     uvicorn.run("main.app", port=8000, reload=True)
